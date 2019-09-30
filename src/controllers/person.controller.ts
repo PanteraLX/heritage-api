41121f2057b1c4982078af7524dd73b4c859b0e0
Samuel Heinzmann <samuel.heinzmann@iml.unibe.ch>
2019-09-30 22:43:03 +0200

    Test

--- src/controllers/person.controller.ts
+++ src/controllers/person.controller.ts
@@ -4,55 +4,56 @@ import { DataTablesResponse, PersonService } from '../services/person.service';
 
 export class PersonController {
 
-  constructor(private personService: PersonService) {
-  }
-
-  public async getPerson(req: Request, res: Response): Promise<Response> {
-    try {
-      const key: string = req.params.key || '';
-      const person: IPerson = await this.personService.getPerson<IPerson>(key);
-      return res.json(person);
-    } catch (error) {
-      res.status(400).json({message: error.message});
+    constructor(private personService: PersonService) {
     }
-  }
 
-  public async getPersons(req: Request, res: Response): Promise<Response> {
-    try {
-      const persons: IPerson[] = await this.personService.getPersons<IPerson>();
-      return res.json(persons);
-    } catch (error) {
-      res.status(400).json({message: error.message});
+    public async getPerson(req: Request, res: Response): Promise<Response> {
+        try {
+            const key: string = req.params.key || '';
+            const person: IPerson = await this.personService.getPerson<IPerson>(key);
+            return res.json(person);
+        } catch (error) {
+            res.status(400).json({message: error.message});
+        }
     }
-  }
 
-  public async getPersonsByQuery(req: Request, res: Response): Promise<Response> {
-    try {
-      const persons: DataTablesResponse<IPerson> = await this.personService.getPersonsByQuery<IPerson>(req.body);
-      return res.json(persons);
-    } catch (error) {
-      res.status(400).json({message: error.message});
+    public async getPersons(req: Request, res: Response): Promise<Response> {
+        try {
+            const persons: IPerson[] = await this.personService.getPersons<IPerson>();
+            return res.json(persons);
+        } catch (error) {
+            res.status(400).json({message: error.message});
+        }
     }
-  }
 
-  public async updatePerson(req: Request, res: Response): Promise<Response> {
-    try {
-      const body: IPerson = req.body;
-      const person: IPerson = await this.personService.updatePerson(body);
-      return res.json(person);
-    } catch (error) {
-      res.status(400).json({message: error.message});
+    public async getPersonsByQuery(req: Request, res: Response): Promise<Response> {
+        try {
+            // @ts-ignore
+            const persons: any = await this.personService.getPersonsByQuery<any>(req.body);
+            return res.json(persons);
+        } catch (error) {
+            res.status(400).json({message: error.message});
+        }
     }
-  }
 
-  public async addPerson(req: Request, res: Response): Promise<Response> {
-    try {
-      const body: IPerson = req.body;
-      const person: IPerson = await this.personService.addPerson<IPerson>(body);
-      return res.json(person);
-    } catch (error) {
-      res.status(400).json({message: error.message});
+    public async updatePerson(req: Request, res: Response): Promise<Response> {
+        try {
+            const body: IPerson = req.body;
+            const person: IPerson = await this.personService.updatePerson(body);
+            return res.json(person);
+        } catch (error) {
+            res.status(400).json({message: error.message});
+        }
     }
-  }
+
+    public async addPerson(req: Request, res: Response): Promise<Response> {
+        try {
+            const body: IPerson = req.body;
+            const person: IPerson = await this.personService.addPerson<IPerson>(body);
+            return res.json(person);
+        } catch (error) {
+            res.status(400).json({message: error.message});
+        }
+    }
 }
 
--- src/controllers/search.controller.ts
+++ src/controllers/search.controller.ts
@@ -4,25 +4,25 @@ import { IUser } from '../models/user';
 import { SearchService } from '../services/search.service';
 
 export class SearchController {
-  constructor(private searchService: SearchService) {
-  }
-
-  public async search(req: Request, res: Response): Promise<Response> {
-    try {
-      const children: IPerson[] = await this.searchService.search(req.body);
-      return res.json(children);
-    } catch (error) {
-      res.status(400).json({message: error.message});
+    constructor(private searchService: SearchService) {
     }
-  }
 
-  public async simpleSearch(req: Request, res: Response): Promise<Response> {
-    try {
-      const query: string = req.query.query || '';
-      const persons: IPerson[] = await this.searchService.simpleSearch(query);
-      return res.json(persons);
-    } catch (error) {
-      res.status(400).json({message: error.message});
+    public async search(req: Request, res: Response): Promise<Response> {
+        try {
+            const children: IPerson[] = await this.searchService.search(req.body);
+            return res.json(children);
+        } catch (error) {
+            res.status(400).json({message: error.message});
+        }
     }
-  }
+
+    public async simpleSearch(req: Request, res: Response): Promise<Response> {
+        try {
+            const query: string = req.query.query || '';
+            const persons: IPerson[] = await this.searchService.simpleSearch(query);
+            return res.json(persons);
+        } catch (error) {
+            res.status(400).json({message: error.message});
+        }
+    }
 }
